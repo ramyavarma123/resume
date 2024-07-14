143f46587de40d8ec20d4e97f9a42f326f509412
ramya <ramya@gmail.com>
2024-07-14 07:21:00 +0530

    hello world

---
+++ multiplystring.py
@@ -0,0 +1,6 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        x1=int(num1)
+        x2=int(num2)
+        x3=x1*x2
+        return str(x3)     
---
+++ pow(x,n).py
@@ -0,0 +1,4 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return float(pow(x,n))
+        
